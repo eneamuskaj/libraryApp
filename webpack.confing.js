33f2aa13b5e916b37b4f9ec919757598a3ac319b
eneamuskaj <eneamuskaj@gmail.com>
2021-11-22 16:01:58 -0600

    added firebase as backend2

--- webpack.confing.js
+++ webpack.confing.js
@@ -3,8 +3,7 @@ const path = require("path");
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/dist/",
+    path: "/",
     filename: "bundle.js",
   },
   devtool: "eval-source-map",
